3a2ff8f69316de34b4217d6157dadb692cb93f95
Rahul <snakepowerpoint@gmail.com>
2020-05-09 11:43:15 +0800

    change number of worker to avoid bug

--- data/datamgr.py
+++ data/datamgr.py
@@ -55,7 +55,7 @@ class SimpleDataManager(DataManager):
   def get_data_loader(self, data_file, aug): #parameters that would change on train/val set
     transform = self.trans_loader.get_composed_transform(aug)
     dataset = SimpleDataset(data_file, transform)
-    data_loader_params = dict(batch_size = self.batch_size, shuffle = True, num_workers = 4, pin_memory = True)
+    data_loader_params = dict(batch_size = self.batch_size, shuffle = True, num_workers = 0, pin_memory = True)
     data_loader = torch.utils.data.DataLoader(dataset, **data_loader_params)
 
     return data_loader
@@ -78,6 +78,6 @@ class SetDataManager(DataManager):
     else:
       dataset = SetDataset( data_file , self.batch_size, transform )
       sampler = EpisodicBatchSampler(len(dataset), self.n_way, self.n_eposide )
-    data_loader_params = dict(batch_sampler = sampler,  num_workers=4)
+    data_loader_params = dict(batch_sampler = sampler,  num_workers=0)
     data_loader = torch.utils.data.DataLoader(dataset, **data_loader_params)
     return data_loader
